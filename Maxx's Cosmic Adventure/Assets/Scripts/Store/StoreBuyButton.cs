617a7f457a3c2db88271d3b4e28803764bb4236c
Aharon Smbatyan <aharon91@mail.ru>
2013-05-08 22:45:39 +0400

    When clicking upgrade powerup button in store having no enough unlikeliums to upgrades then buy unlikeliums popup appears.

--- Maxx's Cosmic Adventure/Assets/Scripts/Store/StoreBuyButton.cs
+++ Maxx's Cosmic Adventure/Assets/Scripts/Store/StoreBuyButton.cs
@@ -7,14 +7,21 @@ public class StoreBuyButton : MonoBehaviour {
 	{
 		if(!isDown)
 		{
+			if(Store.Instance._currentPowerup.FullyUpdated) return;
+			
 			var cost = Store.Instance._currentPowerup.stored?Store.Instance.costStored:
 				Store.Instance.costs[Store.Instance._currentPowerup.level];
 			
-			if(!Store.Instance._currentPowerup.FullyUpdated && Store.Instance.Unlikeliums>=cost)
+			if(Store.Instance.Unlikeliums>=cost)
 			{
 				Store.Instance.Unlikeliums -= cost;
 				Store.Instance._currentPowerup.level++;
 				Store.Instance.Activate(Store.Instance._currentPowerup);		
+			}
+			else
+			{
+				Store.Instance.popupUpgradePowerups.SetActive(false);
+				Store.Instance.popupBuyUnlikeliums.SetActive(true);
 			}
 		}
 	}
