373a78cc5ac516b74f3adcb9c45c69c2316bf111
Aharon Smbatyan <aharon91@mail.ru>
2013-05-20 18:52:20 +0400

    Fixed The button back to Options from Debug Options still says "DEBUG"

--- Maxx's Cosmic Adventure/Assets/Scripts/Store, Options/OptionsDebug.cs
+++ Maxx's Cosmic Adventure/Assets/Scripts/Store, Options/OptionsDebug.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 using System.Collections;
 
 public class OptionsDebug : MonoBehaviour {
-
+	
+	public UILabel buttonName;
 	void OnClick()
 	{
 		Options.Instance.debug = !Options.Instance.debug;
+		buttonName.text = Options.Instance.debug?"OPTIONS":"DEBUG";
 	}
 }
