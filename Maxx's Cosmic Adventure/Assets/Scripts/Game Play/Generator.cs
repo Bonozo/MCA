414526a061a565e2ac2e7fa37ae1986393043987
Aharon Smbatyan <aharon91@mail.ru>
2013-05-26 02:42:30 +0400

    Updated Jeebie attacks. more jeebies, first jeebie spawns after 500 meters. next jeebie spawn delays 100,150,200 or 300 meters. fixes.
    Created levels by segments of distance:
    [500-1000] - single Blue Fighters
    [1000-2000] - single or double Blue Fighters
    [2000-3000] - Blue Fighters or Leaders.
    [3000-4000] - more Blue Fighters or Leaders.
    [4000-5000] - RedKamikaze or Blue Fighter (a Purple with a big probablity)
    [5000-6000] - more RedKamikaze or Blue Fighter
    [6000-7000] - RedKamikaze or TripleKamikaze
    [7000-8000] - PurpleFigher or BlueFighter
    ...
    [19000-20000]
    [20000-22000]
    [22000-24000]
    ...
    [28000-30000]
    [30000-game over]

--- Maxx's Cosmic Adventure/Assets/Scripts/Game Play/Generator.cs
+++ Maxx's Cosmic Adventure/Assets/Scripts/Game Play/Generator.cs
@@ -5,18 +5,23 @@ public class Generator : MonoBehaviour {
 	
 	#region Alien Ship
 	
-	public bool GenerateAlienShip = false;
-	public GameObject[] AlienShipPrefabs;
-	public float AlienShipDistanceMin=30f, AlienShipDistanceMax=50f;
-	public float AlienShipFrontAngleMaxDelta = 30f;
-	
-	
-	public void GenerateNewAlienShip(int index)
+	public enum Jeebie
 	{
-		if( GenerateAlienShip && AlienShip.GlobalCount<30) 
-			Instantiate(AlienShipPrefabs[index]);
+		BlueFighterPilot=0,
+		BlueLeader=1,
+		RedKamikaze=2,
+		TripleKamikaze=3,
+		PurpleFigher=4,
+		Reactive=5
 	}
 	
+	private readonly float first_jeebie_distance = 500f;
+	private readonly float[] next_jeebie_stage_distance = new float[] {100f,150f,200f,300f};
+	private float next_jeebie_distance = 0f;
+	
+	public bool GenerateAlienShip = false;
+	public GameObject[] AlienShipPrefabs;
+	/*
 	public void GenerateNewAlienShip()
 	{
 		float maxdistance = 12000f;
@@ -31,17 +36,213 @@ public class Generator : MonoBehaviour {
 		if(index==2||index==3) count = Mathf.Min(count,3);
 		else if(index==0&&count==2&&Random.Range(0,2)==0) delta=0f;
 
-		StartCoroutine(StartAlienAttack(index,count,delta));
+		StartCoroutine(StartAlienAttack((Jeebie)index,count,delta));
+	}
+	*/
+	
+	public void SpawnJeebieByDistance()
+	{
+		float distance = LevelInfo.Environments.playerShip.DistanceTravelled;
+		
+		if(distance<=1000f)
+		{
+			StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,1,0));
+		}
+		else if(distance<=2000f)
+		{
+			StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(1,3),0f));
+		}
+		else if(distance<=3000f)
+		{
+			if(Random.Range(0,3)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.BlueLeader,1,0f));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(1,3),Random.Range(0.5f,1f)));
+		}
+		else if(distance<=4000f)
+		{
+			if(Random.Range(0,2)==1)//50%
+				StartCoroutine(StartAlienAttack(Jeebie.BlueLeader,1,0f));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(1,4),0.5f));
+		}
+		else if(distance<=5000f)
+		{
+			if(Random.Range(0,2)==1)//50%
+				StartCoroutine(StartAlienAttack(Jeebie.RedKamikaze,Random.Range(1,4),Random.Range(0.3f,0.5f)));
+			else if(Random.Range(0,2)==1)//25%
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,1,0f));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(1,5),0.5f));
+		}
+		else if(distance<=6000f)
+		{
+			if(Random.Range(0,3)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(2,5),0.5f));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.RedKamikaze,Random.Range(2,5),Random.Range(0.3f,0.5f)));	
+		}
+		else if(distance<=7000f)
+		{
+			if(Random.Range(0,3)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.TripleKamikaze,1,0f));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.RedKamikaze,Random.Range(3,7),Random.Range(0.3f,0.5f)));	
+		}
+		else if(distance<=8000f)
+		{
+			if(Random.Range(0,3)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,1,0f));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(4,8),Random.Range(0.3f,0.5f)));	
+		}
+		else if(distance<=9000f)
+		{
+			if(Random.Range(0,3)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.BlueLeader,1,0f));
+			else 
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(1,3),0f));
+		}
+		else if(distance<=10000f)// Purple Attack
+		{
+			if(Random.Range(0,2)==1)//50%
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(1,3),0.2f));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(2,5),0.7f));
+		}
+		else if(distance<=11000f)
+		{
+			if(Random.Range(0,3)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(2,5),0.3f));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(6,10),Random.Range(0.3f,0.5f)));
+		}
+		else if(distance<=12000f)// Blue Fighter and Leader Attack
+		{
+			if(Random.Range(0,2)==1)//50%
+				StartCoroutine(StartAlienAttack(Jeebie.BlueLeader,2,Random.Range(1f,2f)));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(7,12),0.5f));	
+		}
+		else if(distance<=13000f) // Reactive and Triple Kamikaze Attack
+		{
+			if(Random.Range(0,2)==1)//50%
+				StartCoroutine(StartAlienAttack(Jeebie.Reactive,Random.Range(6,12),Random.Range(0.2f,0.4f)));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.TripleKamikaze,Random.Range(2,5),Random.Range(0.7f,1.2f)));
+		}
+		else if(distance<=14000)
+		{
+			if(Random.Range(0,2)==1)//50%
+				StartCoroutine(StartAlienAttack(Jeebie.Reactive,Random.Range(6,13),Random.Range(0.2f,0.4f)));	
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(6,15),0.5f));	
+		}
+		else if(distance<=15000)
+		{
+			if(Random.Range(0,2)==1)//50%
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(7,15),0.7f));	
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(3,6),Random.Range(0.6f,1.2f)));
+		}
+		else if(distance<=16000)
+		{
+			if(Random.Range(0,2)==1)//50%
+				StartCoroutine(StartAlienAttack(Jeebie.Reactive,Random.Range(10,15),0.7f));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(3,6),Random.Range(0.6f,1.2f)));
+		}
+		else if(distance<=17000)
+		{
+			if(Random.Range(0,3)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.BlueLeader,Random.Range(3,8),1.1f));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(10,15),Random.Range(0.6f,0.9f)));
+		}
+		else if(distance<=18000)
+		{
+			if(Random.Range(0,3)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(7,10),Random.Range(0.8f,1.2f)));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(12,16),Random.Range(0.6f,0.9f)));			
+		}
+		else if(distance<=19000)
+		{
+			if(Random.Range(0,3)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(3,5),Random.Range(0.8f,1.2f)));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(13,18),Random.Range(0.6f,0.9f)));				
+		}
+		else if(distance<=20000)
+		{
+			if(Random.Range(0,3)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.Reactive,Random.Range(7,12),Random.Range(0.3f,1.0f)));
+			else if(Random.Range(0,2)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.RedKamikaze,Random.Range(7,12),Random.Range(0.3f,1.0f)));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.TripleKamikaze,Random.Range(7,10),Random.Range(0.7f,1.2f)));
+		}
+		else if(distance<=22000)
+		{
+			if(Random.Range(0,3)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(7,12),Random.Range(0.3f,1.0f)));
+			else if(Random.Range(0,2)==1)//33%
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(7,12),Random.Range(0.3f,1.0f)));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.BlueLeader,Random.Range(7,10),Random.Range(0.7f,1.2f)));			
+		}
+		else if(distance<=24000)
+		{
+			if(Random.Range(0,2)==1)//50%
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(12,20),Random.Range(0.6f,1.0f)));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.TripleKamikaze,Random.Range(7,10),Random.Range(1f,1.2f)));			
+		}
+		else if(distance<=26000)
+		{
+			if(Random.Range(0,2)==1)//50%
+				StartCoroutine(StartAlienAttack(Jeebie.Reactive,Random.Range(12,20),Random.Range(1f,1.2f)));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(12,20),Random.Range(1f,1.2f)));						
+		}
+		else if(distance<=28000)
+		{
+			if(Random.Range(0,2)==1)//50%
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(15,20),Random.Range(0.7f,1.2f)));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.BlueFighterPilot,Random.Range(7,10),Random.Range(0.8f,1.2f)));			
+		}
+		else if(distance<=30000)
+		{
+			if(Random.Range(0,2)==1)//50%
+				StartCoroutine(StartAlienAttack(Jeebie.PurpleFigher,Random.Range(15,20),Random.Range(0.5f,1.0f)));
+			else
+				StartCoroutine(StartAlienAttack(Jeebie.TripleKamikaze,Random.Range(7,10),Random.Range(0.8f,1.1f)));		
+		}
+		else
+		{
+			var jeeb = (Jeebie)Random.Range(0,AlienShipPrefabs.Length);
+			if(jeeb == Jeebie.TripleKamikaze || jeeb == Jeebie.BlueLeader)
+				StartCoroutine(StartAlienAttack(jeeb,Random.Range(7,10),Random.Range(0.7f,1.5f)));
+			else
+				StartCoroutine(StartAlienAttack(jeeb,Random.Range(15,20),Random.Range(0.3f,1.2f)));
+		}
 	}
 	
-	public IEnumerator StartAlienAttack(int index,int count,float delta)
+	public IEnumerator StartAlienAttack(Jeebie jeebie,int count,float delta)
 	{
-		while(count>0)
+		while(count>0&&GenerateAlienShip)
 		{
 			yield return new WaitForSeconds(delta);
-			LevelInfo.Environments.generator.GenerateNewAlienShip(index);
+			LevelInfo.Environments.generator.SpawnJeebie((int)jeebie);
 			count--;
 		}
+	}
+	
+	public void SpawnJeebie(int index)
+	{
+		if( GenerateAlienShip && AlienShip.GlobalCount<30)
+			Instantiate(AlienShipPrefabs[index]);
 	}
 	
 	#endregion
@@ -91,11 +292,13 @@ public class Generator : MonoBehaviour {
 	private const float Stage_One_Distance = 500f, Stage_Three_Distance = 1000f;
 	
 	private float next_asteroid_time = Stage_One_Step;
-	private float next_jeeble_time = Stage_One_Distance;
 	
 	void Awake()
 	{
+		next_jeebie_distance = first_jeebie_distance + Random.Range(0f,200f);
 		next_unlikelium_distance = Random.Range(100f,150f);
+		
+		
 		next_asteroid_time = Random.Range(160f,300f);
 	}
 	
@@ -121,18 +324,28 @@ public class Generator : MonoBehaviour {
 			/*??*/if(next_asteroid_time<distance) next_asteroid_time=distance+Random.Range(0f,2f);
 		}
 		
-		if( distance >= next_jeeble_time )
+		///////////////// Jeebies //////////////////////
+		if(distance >= next_jeebie_distance)
 		{
+			SpawnJeebieByDistance();
+			var next_distance = next_jeebie_stage_distance[Random.Range(0,next_jeebie_stage_distance.Length)];
+			next_jeebie_distance += next_distance;
+		}
+		////////////////////////////////////////////////
+		
+		/*if( distance >= next_jeeble_time )
+		{
+			Debug.Log("jeebie spawn time");
 			if( Random.Range(0,2)==1 )
 			{
 				//int len = distance >= Stage_Three_Distance ? AlienShipPrefabs.Length : 1 ;
 				GenerateNewAlienShip();
 			}
 			next_jeeble_time += Stage_Two_Step_Enemy;	
-			/*??*/if(next_jeeble_time<distance) next_jeeble_time=distance+Random.Range(0f,2f);
-		}
+			if(next_jeeble_time<distance) next_jeeble_time=distance+Random.Range(0f,2f);
+		}*/
 		
-		// Unlikeliums
+		////////////////// Unlikeliums /////////////////
 		if( distance >= next_unlikelium_distance)
 		{
 			if(distance <= muchUnlikeliumsInFirstTimes_Meter )
@@ -141,6 +354,7 @@ public class Generator : MonoBehaviour {
 				next_unlikelium_distance = distance + Random.Range(distance_Unlikelium_Min,distance_Unlikelium_Max);
 			Instantiate(prefabUnlikelium[Random.Range(0,prefabUnlikelium.Length)]);
 		}
+		////////////////////////////////////////////////
 		
 		if( GenerateUnlikeliumList )
 		{
@@ -176,7 +390,6 @@ public class Generator : MonoBehaviour {
 	public void ResetSpawnDeltaTime()
 	{
 		next_asteroid_time = LevelInfo.Environments.playerShip.DistanceTravelled + Random.Range(0,Stage_One_Step);
-		next_jeeble_time = LevelInfo.Environments.playerShip.DistanceTravelled + Random.Range(0,Stage_Two_Step_Enemy);
 	}
 	
 	#endregion
