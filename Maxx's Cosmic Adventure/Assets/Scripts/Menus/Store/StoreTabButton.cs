92434e5b6b3ba3fce1766a3958d9e77ab53400d7
Aharon Smbatyan <aharon91@mail.ru>
2013-07-30 01:54:13 +0400

    Upgraded the project to Unity 4.2. Updated Red and Green new jeebies. Added Weapons and Ships buttons (tabs) to the Store.

---
+++ Maxx's Cosmic Adventure/Assets/Scripts/Menus/Store/StoreTabButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoreTabButton : MonoBehaviour {
+
+	void OnClick()
+	{
+	}
+	
+	private UIButton button;
+	
+	void Awake()
+	{
+		button = this.GetComponent<UIButton>();
+	}
+	
+	void Update()
+	{
+		button.isEnabled = !Store.Instance.PopupActive;
+	}
+}
